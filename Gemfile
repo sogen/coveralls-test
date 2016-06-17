a1712b1553ea7c8e118d618587dbb93354d32bd9
Genaro Paez <genaro.paez@gteamstaff.com>
2016-06-17 09:35:38 -0500

    undo gem

--- Gemfile
+++ Gemfile
@@ -3,4 +3,3 @@ source 'https://rubygems.org'
 gem "coveralls", "0.8.2"
 gem "rspec"
 gem "rake"
-gem "codeclimate-test-reporter", group: :test, require: nil
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,3 @@ require 'coveralls'
 Coveralls.wear!
 
 require File.join File.dirname(__FILE__), '../run.rb'
-
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
