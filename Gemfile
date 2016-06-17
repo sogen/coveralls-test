87cd22874766fcff10ec5bdb60bc330b72e6fc9c
Genaro Paez <genaro.paez@gteamstaff.com>
2016-06-17 09:29:26 -0500

    codeclimate setup
    adding yml, token, config

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 gem "coveralls", "0.8.2"
 gem "rspec"
 gem "rake"
+gem "codeclimate-test-reporter", group: :test, require: nil
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,6 @@ require 'coveralls'
 Coveralls.wear!
 
 require File.join File.dirname(__FILE__), '../run.rb'
+
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
